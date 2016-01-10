4d749bbcfa9b9c0443249ccbf5b32d8f27a8dfa1
amobiz <amobiz.tw+github@gmail.com>
2016-01-10 22:30:22 +0800

    update schema

--- index.js
+++ index.js
@@ -77,20 +77,91 @@ watch.schema = {
 		},
 		options: {
 			properties: {
-				persistent: {},
-				ignored: {},
-				ignoreInitial: {},
-				followSymlinks: {},
-				cwd: {},
-				usePolling: {},
-				interval: {},
-				binaryInterval: {},
-				useFsEvents: {},
-				alwaysStat: {},
-				depth: {},
-				awaitWriteFinish: {},
-				ignorePermissionErrors: {},
-				atomic: {}
+				persistent: {
+					description: 'Indicates whether the process should continue to run as long as files are being watched.',
+					type: 'boolean',
+					default: true
+				},
+				ignored: {
+					description: 'Defines files/paths to be ignored. The whole relative or absolute path is tested, not just filename.',
+					type: 'array',
+					items: {
+						type: 'string'
+					}
+				},
+				ignoreInitial: {
+					description: 'If set to false then add/addDir events are also emitted for matching paths while instantiating the watching as chokidar discovers these file paths (before the ready event).',
+					type: 'boolean',
+					default: false
+				},
+				followSymlinks: {
+					description: "When false, only the symlinks themselves will be watched for changes instead of following the link references and bubbling events through the link's path.",
+					type: 'boolean',
+					default: true
+				},
+				cwd: {
+					description: 'The base directory from which watch paths are to be derived. Paths emitted with events will be relative to this.',
+					type: 'string'
+				},
+				usePolling: {
+					description: 'Whether to use fs.watchFile (backed by polling), or fs.watch. If polling leads to high CPU utilization, consider setting this to false. It is typically necessary to set this to true to successfully watch files over a network, and it may be necessary to successfully watch files in other non-standard situations. Setting to true explicitly on OS X overrides the useFsEvents default.',
+					type: 'boolean',
+					default: false
+				},
+				interval: {
+					description: 'Interval of file system polling. Polling-specific setting (effective when usePolling: true).',
+					type: 'integer',
+					default: 100
+				},
+				binaryInterval: {
+					description: 'Interval of file system polling for binary files. Polling-specific setting (effective when usePolling: true).',
+					type: 'integer',
+					default: 300
+				},
+				useFsEvents: {
+					description: 'Whether to use the fsevents watching interface if available. When set to true explicitly and fsevents is available this supercedes the usePolling setting. When set to false on OS X, usePolling: true becomes the default.',
+					type: 'boolean'
+				},
+				alwaysStat: {
+					description: "If relying upon the fs.Stats object that may get passed with `add`, `addDir`, and `change` events, set this to true to ensure it is provided even in cases where it wasn't already available from the underlying watch events.",
+					type: 'boolean',
+					default: false
+				},
+				depth: {
+					description: 'If set, limits how many levels of subdirectories will be traversed.',
+					type: 'integer'
+				},
+				awaitWriteFinish: {
+					description: 'In some cases, especially when watching for large files there will be a need to wait for the write operation to finish before responding to a file creation or modification. Setting awaitWriteFinish to true (or a truthy value) will poll file size, holding its add and change events until the size does not change for a configurable amount of time.',
+					anyOf: [{
+						type: 'boolean',
+						default: false
+					}, {
+						type: 'object',
+						properties: {
+							stabilityThreshold: {
+								description: 'Amount of time in milliseconds for a file size to remain constant before emitting its event.',
+								type: 'integer',
+								default: 2000
+							},
+							pollInterval: {
+								description: 'File size polling interval.',
+								type: 'integer',
+								default: 100
+							}
+						}
+					}]
+				},
+				ignorePermissionErrors: {
+					description: "Indicates whether to watch files that don't have read permissions if possible. If watching fails due to EPERM or EACCES with this set to true, the errors will be suppressed silently.",
+					type: 'boolean',
+					default: false
+				},
+				atomic: {
+					description: 'Automatically filters out artifacts that occur when using editors that use "atomic writes" instead of writing directly to the source file. (default: true if useFsEvents and usePolling are false.)',
+					type: 'boolean',
+					default: false
+				}
 			}
 		}
 	}

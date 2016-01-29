cf600532cbeab18ea03e993dc311429c5882c6bd
amobiz <amobiz.tw+github@gmail.com>
2016-01-29 09:25:59 +0800

    document readme

--- README.md
+++ README.md
@@ -1,44 +1,69 @@
 # gulp-ccr-watch
+
 Watch source files of specific tasks and their descendants and run corresponding task when a file changes.
 A cascading configurable gulp recipe for [gulp-chef](https://github.com/gulp-cookery/gulp-chef).
 
 ## Install
+
 ```bash
-npm install gulp-ccr-watch
+$ npm install --save-dev gulp-chef gulp-ccr-watch
 ```
-## Configuration
+
+## Recipe
+
+Do on Change
+
+## Ingredients
+
+* [browser-sync](https://github.com/BrowserSync/browser-sync)
+
+* [gulp.watch()](https://github.com/gulpjs/gulp/blob/4.0/docs/API.md#gulpwatchglob-opts-fn)
+
+## Type
+
+[Flow Controller](https://github.com/gulp-cookery/gulp-chef#writing-flow-controller)
+
+## API
+
 File watching is provided by the [chokidar](https://github.com/paulmillr/chokidar) module. Please report any file watching problems directly to its [issue tracker](https://github.com/paulmillr/chokidar/issues).
 
-### options
+### config.options
+
 Options that are passed to chokidar. See [chokidar's API](https://github.com/paulmillr/chokidar#api) for options.
 
-### task
+### config.task
+
 Tasks to take care of.
 
-## Configuration Example
-```javascript
-{
-    scripts: {
-        src: '**/*.js'
-        task: function (done) {}
+## Usage
+
+``` javascript
+var gulp = require('gulp');
+var chef = require('gulp-chef');
+var browserSync = require('browser-sync');
+
+var meals = chef({
+    src: 'app/',
+    dest: 'dist/',
+    markups: {
+        src: '**/*.html',
+        recipe: 'copy'
     },
     styles: {
-        src: '**/*.css',
-        task: function (done) {}
+        src: '**/*.less',
+        plugin: 'gulp-less',
+        spit: true
     },
-    build: ['scripts', 'styles'],
-    watch: ['scripts', 'styles'],
-    build_watch: {
-        description: 'build and then watch',
-        series: ['build', 'watch'],
-    },
-    with_options: {
-        watch: {
-            options: {
-                usePolling: true
-            },
-            task: ['scripts', 'styles']
+    browserSync: {
+        plugin: 'browser-sync',
+        options: {
+            server: '{{dest.path}}'
         }
-    }
-}
+    },
+    build: ['markups', 'styles'],
+    watch: ['markups', 'styles'],
+    serve: ['build', 'browserSync', 'watch']
+});
+
+gulp.registry(meals);
 ```
--- index.js
+++ index.js
@@ -1,57 +1,6 @@
 'use strict';
 
-/**
- * Watch
- *   Watch source files of specific tasks and their descendants and run corresponding task when a file changes.
- *
- * zh_TW：
- *   watch 只要指定相依的 task 即可。
- *   然後自動由相依的 task 找出需要監控的對應檔案，並在檔案變動時，自動執行對應的 task。
- *
- * Recipe:
- *   watch
- *
- * Ingredients:
- *   browser-sync
- *   gulp.watch()
- *
- */
-function watch() {
-	var browserSync = require('browser-sync');
-	var log = require('gulplog');
-
-	var gulp = this.gulp;
-	var config = this.config;
-	var options = config.options || {};
-	var tasks = this.tasks || [];
-
-	tasks.forEach(function (task) {
-		var src;
-
-		src = sources([], task);
-		if (src.length) {
-			log.info('watch', 'watching ' + JSON.stringify(src) + ' for ' + task.displayName);
-			gulp.watch(src, options, task).on('all', browserSync.reload);
-		} else {
-			log.warn('watch', 'warning', task.displayName + ' has no `src` property.');
-		}
-	});
-
-	// recursively add child task's sources.
-	function sources(result, task) {
-		var ret = result;
-
-		if (task.config && task.config.src) {
-			ret = ret.concat(task.config.src.globs);
-		}
-		if (task.tasks) {
-			ret = task.tasks.reduce(sources, ret);
-		}
-		return ret;
-	}
-}
-
-watch.schema = {
+var schema = {
 	title: 'watch',
 	description: 'Watch source files of specific tasks and their descendants and run corresponding task when a file changes.',
 	type: 'object',
@@ -167,6 +116,41 @@ watch.schema = {
 	}
 };
 
-watch.type = 'flow';
+function watch() {
+	var browserSync = require('browser-sync');
+	var log = require('gulplog');
+
+	var gulp = this.gulp;
+	var config = this.config;
+	var options = config.options || {};
+	var tasks = this.tasks || [];
+
+	tasks.forEach(function (task) {
+		var src;
+
+		src = sources([], task);
+		if (src.length) {
+			log.info('watch', 'watching ' + JSON.stringify(src) + ' for ' + task.displayName);
+			gulp.watch(src, options, task).on('all', browserSync.reload);
+		} else {
+			log.warn('watch', 'warning', task.displayName + ' has no `src` property.');
+		}
+	});
+
+	// recursively add child task's sources.
+	function sources(result, task) {
+		var ret = result;
+
+		if (task.config && task.config.src) {
+			ret = ret.concat(task.config.src.globs);
+		}
+		if (task.tasks) {
+			ret = task.tasks.reduce(sources, ret);
+		}
+		return ret;
+	}
+}
 
 module.exports = watch;
+module.exports.schema = schema;
+module.exports.type = 'flow';
